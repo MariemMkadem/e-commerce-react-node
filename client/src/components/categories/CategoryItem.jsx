47e3fd8c723d3fd8a694a519c60afefce2c2c31c
Mariem Mkadem <mmkadem@mobelite.fr>
2022-04-28 14:06:48 +0100

    separate files

--- client/src/components/categories/CategoryItem.jsx
+++ client/src/components/categories/CategoryItem.jsx
@@ -1,47 +1,5 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
-import { mobile } from "../../responsive";
-
-const Container = styled.div`
-  flex: 1;
-  margin: 3px;
-  height: 70vh;
-  position: relative;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  ${mobile({ height: "20vh" })}
-
-`;
-
-const Info = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Title = styled.h1`
-    color:white;
-    margin-bottom: 20px;
-`;
-
-const Button = styled.button`
-    border:none;
-    padding: 10px;
-    background-color: white;
-    color:gray;
-    cursor: pointer;
-    font-weight: 600;
-`;
+import {Container, Image, Info, Button, Title} from './CategoriItemWrapper';
 
 const CategoryItem = ({ item }) => {
   return (
